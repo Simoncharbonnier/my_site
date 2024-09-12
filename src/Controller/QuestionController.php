a7cee8d35331db6f4a14ce2f3f616fabad42bfc5
Simoncharbonnier <simoncharbonnier@orange.fr>
2024-09-12 12:55:43 +0200

    feat: question page & answers

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,28 +3,67 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\QuestionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\AnswerFormType;
+use App\Entity\Answer;
 
 class QuestionController extends AbstractController
 {
     /**
      * Question of the day page
+     * @param string $month month
+     * @param string $day day
      * @param QuestionRepository $questionRepository question repository
+     * @param Request $request request
+     * @param EntityManagerInterface $entityManager entity manager
      */
-    #[Route('/question', name: 'app_question')]
-    public function index(QuestionRepository $questionRepository): Response
+    #[Route('/question/{month}/{day}', name: 'app_question')]
+    public function question(
+        string $month,
+        string $day,
+        QuestionRepository $questionRepository,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
     {
-        $question = $questionRepository->findOneBy(['day' => date('d'), 'month' => date('m')]);
+        $question = $questionRepository->findOneBy(['month' => $month, 'day' => $day]);
 
-        if (empty($question)) {
-            $this->addFlash('danger', 'Il n\'y a pas de question aujourd\'hui, contacte Simon pour plus d\'infos !');
+        $alreadyAnswered = false;
+        foreach ($question->getAnswers() as $answer) {
+            if ($answer->getUser() !== $this->getUser()) {
+                $question->removeAnswer($answer);
+            } else if ($answer->getYear() === intval(date('Y'))) {
+                $alreadyAnswered = true;
+            }
+        }
+
+        $form = $this->createForm(AnswerFormType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newAnswer = new Answer();
+            $newAnswer->setContent($form->get('content')->getData());
+            $newAnswer->setYear(intval(date('Y')));
+            $newAnswer->setQuestion($question);
+            $newAnswer->setUser($this->getUser());
+
+            $entityManager->persist($newAnswer);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Vous avez répondu à la question du jour !');
+            return $this->redirectToRoute('app_question', ['month' => $month, 'day' => $day]);
         }
 
         return $this->render('question/index.html.twig', [
-            'controller_name' => 'QuestionController',
-            'question' => $question
+            'today_question' => date('m') === $month && date('d') === $day,
+            'day' => $day,
+            'month' => $month,
+            'question' => $question,
+            'form' => $form,
+            'already_answered' => $alreadyAnswered
         ]);
     }
 }
