ad62d4c302d6e2ebd85088576e1bdbb165a07a5a
Simoncharbonnier <simoncharbonnier@orange.fr>
2024-09-15 16:09:09 +0200

    feat: date formatter

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -10,6 +10,10 @@ use App\Repository\QuestionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Form\AnswerFormType;
 use App\Entity\Answer;
+use App\Service\DateFormatter;
+
+use App\Entity\Question;
+
 
 class QuestionController extends AbstractController
 {
@@ -20,6 +24,7 @@ class QuestionController extends AbstractController
      * @param QuestionRepository $questionRepository question repository
      * @param Request $request request
      * @param EntityManagerInterface $entityManager entity manager
+     * @param DateFormatter $dateFormatter date formatter
      */
     #[Route('/question/{month}/{day}', name: 'app_question')]
     public function question(
@@ -27,10 +32,17 @@ class QuestionController extends AbstractController
         string $day,
         QuestionRepository $questionRepository,
         Request $request,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        DateFormatter $dateFormatter
     ): Response
     {
         $question = $questionRepository->findOneBy(['month' => $month, 'day' => $day]);
+        $date = $dateFormatter->format($month, $day);
+
+        if (!$question) {
+            $this->addFlash('success', 'Il n\'y a pas de question pour le '.$date.'.');
+            return $this->redirectToRoute('app_home');
+        }
 
         $alreadyAnswered = false;
         foreach ($question->getAnswers() as $answer) {
@@ -59,8 +71,7 @@ class QuestionController extends AbstractController
 
         return $this->render('question/index.html.twig', [
             'today_question' => date('m') === $month && date('d') === $day,
-            'day' => $day,
-            'month' => $month,
+            'date' => $date,
             'question' => $question,
             'form' => $form,
             'already_answered' => $alreadyAnswered
